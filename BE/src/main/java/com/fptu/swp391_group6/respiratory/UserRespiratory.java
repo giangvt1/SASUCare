85922dd1feeed727030124e9c4f9351c2dc66a6f
giangvt1 <giangcr7dz@gmail.com>
2025-01-20 14:56:18 +0700

    fix

--- BE/src/main/java/com/fptu/swp391_group6/respiratory/UserRespiratory.java
+++
@@ -1,9 +0,0 @@
-package com.fptu.swp391_group6.respiratory;
-
-import com.fptu.swp391_group6.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRespiratory extends JpaRepository<User, String> {
-    User findByUsername(String username);
-    boolean existsByUsername(String username);
-}
