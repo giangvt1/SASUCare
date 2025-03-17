8318a94b035107b3a0da7fe706746623b6ec5b27
pvchiu <chieupham1011@gmail.com>
2025-03-18 01:21:36 +0700

    update link web

--- src/java/controller/customerService/CreatePostController.java
+++ src/java/controller/customerService/CreatePostController.java
@@ -2,10 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controller.HRController;
+package controller.customerService;
 
-import static controller.HRController.CreatePostController.UPLOAD_IMAGES_DIR;
-import static controller.HRController.CreatePostController.extractFileName;
 import dao.PostDAO;
 import dao.UserDBContext;
 import java.io.IOException;
@@ -23,8 +21,6 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import model.Post;
 import model.system.Staff;
 import model.system.User;
@@ -33,15 +29,13 @@ import model.system.User;
  *
  * @author admin
  */
-@WebServlet(name = "EditPostController", urlPatterns = {"/hr/edit-post"})
+@WebServlet(name = "CreatePostController", urlPatterns = {"/hr/create-post"})
 @MultipartConfig(
         fileSizeThreshold = 1024 * 1024, // 1 MB
         maxFileSize = 1024 * 1024 * 5, // 5 MB
         maxRequestSize = 1024 * 1024 * 10 // 10 MB
 )
-public class EditPostController extends HttpServlet {
-
-    private static final Logger LOGGER = Logger.getLogger(EditPostController.class.getName());
+public class CreatePostController extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -55,21 +49,7 @@ public class EditPostController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try {
-            String postId = request.getParameter("id");
-
-            PostDAO dao = new PostDAO();
-            Post p = dao.get(postId);
-            if (p == null) {
-                response.sendRedirect(request.getContextPath() + "/hr/posts");
-                return;
-            }
-
-            request.setAttribute("p", p);
-            request.getRequestDispatcher("/hr/EditPost.jsp").forward(request, response);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        request.getRequestDispatcher("/customer_service/CreatePost.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -95,6 +75,8 @@ public class EditPostController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    public static final String UPLOAD_IMAGES_DIR = "images";
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -102,9 +84,6 @@ public class EditPostController extends HttpServlet {
             // Lấy và trim các giá trị đầu vào
             String title = request.getParameter("title") != null ? request.getParameter("title").trim() : "";
             String content = request.getParameter("content") != null ? request.getParameter("content").trim() : "";
-            boolean status = request.getParameter("status").equals("1") ? true : false;
-            int id = Integer.parseInt(request.getParameter("id"));
-            String oldImage = request.getParameter("oldImage");
 
             // Xử lý file image upload
             Part filePart = request.getPart("image");
@@ -132,20 +111,28 @@ public class EditPostController extends HttpServlet {
                 }
                 //get link image
                 imageFile = UPLOAD_IMAGES_DIR + File.separator + fileName;
-            } else {
-                imageFile = oldImage;
             }
 
             HttpSession session = request.getSession();
             Staff staff = (Staff) session.getAttribute("staff");
             PostDAO dao = new PostDAO();
-            Post post = new Post(id, title, content, staff.getId(), imageFile, status);
-            dao.update(post);
+            Post post = new Post(title, content, staff.getId(), imageFile);
+            dao.insert(post);
             request.getRequestDispatcher("/hr/posts").forward(request, response);
         } catch (Exception e) {
             e.printStackTrace();
-            LOGGER.log(Level.SEVERE, "Error updating post", e.getMessage());
         }
+    }
+
+    public static String extractFileName(Part part) {
+        String contentDisposition = part.getHeader("content-disposition");
+        String[] tokens = contentDisposition.split(";");
+        for (String token : tokens) {
+            if (token.trim().startsWith("filename")) {
+                return token.substring(token.indexOf('=') + 2, token.length() - 1);
+            }
+        }
+        return "";
     }
 
     /**
